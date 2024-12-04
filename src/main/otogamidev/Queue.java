1974b36fb1bc85940b1c6366606605b33d4f63ba
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2024-12-03 23:17:14 -0300

    Implementação do método getObjectType na classe Queue e ajustes nos logs dos demais métodos

--- src/main/otogamidev/Queue.java
+++ src/main/otogamidev/Queue.java
@@ -1,7 +1,5 @@
 package main.otogamidev;
 
-import java.util.Arrays;
-
 /**
  * Classe responsavel por uma implementacao de Fila.
  *
@@ -40,7 +38,7 @@ public class Queue {
      */
     public void setAllElements(final Object[] elements) {
         for(int index = 0; elements.length > index; index++) {
-            System.out.println(CLASS_NAME.concat("setAllElements(): elements = " + String.valueOf(elements[index])));
+            System.out.println(CLASS_NAME.concat(" - setAllElements(): elements[" + index + "] = " + getObjectType(elements[index])));
         }
         this.elements = elements;
     }
@@ -50,8 +48,8 @@ public class Queue {
      * @param element elemento que sera adicionado.
      */
     public void addElement(final Object element) {
-        System.out.println(CLASS_NAME.concat("addElement(): BEGIN"));
-        System.out.println(CLASS_NAME.concat("addElement(): element = " + String.valueOf(element)));
+        System.out.println("\n".concat(CLASS_NAME).concat(" - addElement(): BEGIN"));
+        System.out.println(CLASS_NAME.concat(" - addElement(): element = " + getObjectType(element)));
         final int actualSize = getSize();
         final int sizeNewElements = isEmpty() ? 0 : actualSize + 1;
         Object[] newElements = new Object[sizeNewElements];
@@ -66,7 +64,8 @@ public class Queue {
         }
 
         setAllElements(newElements);
-        System.out.println(CLASS_NAME.concat("addElement(): END"));
+        System.out.println(CLASS_NAME.concat(" - addElement(): END"));
+        System.out.println(CLASS_NAME.concat(" ===================="));
     }
 
     /**
@@ -76,7 +75,7 @@ public class Queue {
      * @return Retorna o indice do elemento encontrado no array. Se nao encontrar, retorna o valor -1.
      */
     public int searchElement(final Object[] array, final Object element) {
-        System.out.println(CLASS_NAME.concat("searchElement(): BEGIN"));
+        System.out.println("\n".concat(CLASS_NAME).concat(" - searchElement(): BEGIN"));
 
         final int arrayLength = array.length;
         if(arrayLength == 0) throw new ArrayIndexOutOfBoundsException(arrayLength);
@@ -90,11 +89,81 @@ public class Queue {
             }
         }
 
-        if(foundElementIndex == NOT_FOUND) System.out.println(CLASS_NAME.concat("searchElement(): Element not found."));
+        if(foundElementIndex == NOT_FOUND) System.out.println(CLASS_NAME.concat(" - searchElement(): Element not found."));
 
-        System.out.println(CLASS_NAME.concat("searchElement(): " + foundElementIndex));
-        System.out.println(CLASS_NAME.concat("searchElement(): END"));
+        System.out.println(CLASS_NAME.concat(" - searchElement(): " + foundElementIndex));
+        System.out.println(CLASS_NAME.concat(" - searchElement(): END"));
+        System.out.println(CLASS_NAME.concat(" ===================="));
         return foundElementIndex;
+    }
+
+    /**
+     * Metodo responsável por identificar a instancia do elemento.
+     * @param object elemento a ser identificado
+     * @return Retorna o nome do tipo do elemento
+     */
+    public String getObjectType(final Object object) {
+        System.out.println("\n".concat(CLASS_NAME).concat(" - getObjectType(): BEGIN"));
+        String objectType = "";
+        if(object instanceof Integer){
+            objectType = "Integer";
+        } else if (object instanceof Double) {
+            objectType = "Double";
+        } else if (object instanceof Float) {
+            objectType = "Float";
+        } else if (object instanceof Long) {
+            objectType = "Long";
+        } else if (object instanceof Short) {
+            objectType = "Short";
+        } else if (object instanceof Byte) {
+            objectType = "Byte";
+        } else if (object instanceof Character) {
+            objectType = "Character";
+        } else if (object instanceof Boolean) {
+            objectType = "Boolean";
+        } else if (object instanceof String) {
+            objectType = "String";
+        } else if(object instanceof Integer[]){
+            objectType = "Integer[]";
+        } else if (object instanceof Double[]) {
+            objectType = "Double[]";
+        } else if (object instanceof Float[]) {
+            objectType = "Float[]";
+        } else if (object instanceof Long[]) {
+            objectType = "Long[]";
+        } else if (object instanceof Short[]) {
+            objectType = "Short[]";
+        } else if (object instanceof Byte[]) {
+            objectType = "Byte[]";
+        } else if (object instanceof Character[]) {
+            objectType = "Character[]";
+        } else if (object instanceof Boolean[]) {
+            objectType = "Boolean[]";
+        } else if (object instanceof String[]) {
+            objectType = "String[]";
+        } else if(object instanceof int[]){
+            objectType = "integer[]";
+        } else if (object instanceof double[]) {
+            objectType = "double[]";
+        } else if (object instanceof float[]) {
+            objectType = "float[]";
+        } else if (object instanceof long[]) {
+            objectType = "long[]";
+        } else if (object instanceof short[]) {
+            objectType = "short[]";
+        } else if (object instanceof byte[]) {
+            objectType = "byte[]";
+        } else if (object instanceof char[]) {
+            objectType = "char[]";
+        } else if (object instanceof boolean[]) {
+            objectType = "boolean[]";
+        } else {
+            objectType = "NONE";
+            System.out.println(CLASS_NAME.concat(" - getObjectType(): Object type does not found"));
+        }
+        System.out.println(CLASS_NAME.concat(" - getObjectType(): END"));
+        System.out.println(CLASS_NAME.concat(" ===================="));
+        return objectType;
     }
 
     public static void main(String[] args) {

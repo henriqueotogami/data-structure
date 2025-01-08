5f320183f59052a8b74401b34df905a8f4e37992
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2025-01-07 23:58:07 -0300

    Ajustes nos logs dos testes, substituindo system.out pelo log4j

--- src/test/otogamidev/queue/QueueTest.java
+++ src/test/otogamidev/queue/QueueTest.java
@@ -2,7 +2,10 @@ package test.otogamidev.queue;
 
 import main.otogamidev.queue.utils.DefaultValues;
 import main.otogamidev.queue.Queue;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.*;
+
 
 /**
  * Classe responsavel pelos testes da classe Queue (Fila), dos estudos de estruturas de dados.
@@ -12,11 +15,16 @@ import org.junit.jupiter.api.*;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class QueueTest {
 
-    final String CLASS_NAME = this.getClass().getName();
+    private static final String CLASS_NAME = "QueueTest";
 
+    private final Logger logger = LogManager.getLogger(QueueTest.class);
     private final Queue queue = new Queue();
+
     private final int QUEUE_MAX_SIZE = 34;
 
+    /**
+     * @return Retorna um índice "aleatorio" para que os metodos dos testes abaixos utilizem tipos de objetos diferentes.
+     */
     private int getRandomIndexQueueSize() {
         final int size = queue.getSize();
         final int randomIndex = (int) (Math.random() * size);
@@ -30,11 +38,11 @@ class QueueTest {
     @Test
     @Order(1)
     void createQueueTest() {
-        System.out.println(CLASS_NAME.concat(" - createQueueTest(): BEGIN"));
+        logger.debug(CLASS_NAME.concat(" - createQueueTest(): BEGIN"));
         final Queue queue1 = new Queue();
         final boolean isEmpty = queue1.isEmpty();
-        System.out.println(CLASS_NAME + " - createQueueTest(): isEmtpy = " + isEmpty);
-        System.out.println(CLASS_NAME.concat(" - createQueueTest(): END"));
+        logger.info(CLASS_NAME + " - createQueueTest(): isEmtpy = " + isEmpty);
+        logger.debug(CLASS_NAME.concat(" - createQueueTest(): END"));
         Assertions.assertTrue(isEmpty);
     }
 
@@ -45,10 +53,10 @@ class QueueTest {
     @Test
     @Order(2)
     void addElementTest() {
-        System.out.println(CLASS_NAME.concat(" - addElementTest(): BEGIN"));
+        logger.debug(CLASS_NAME.concat(" - addElementTest(): BEGIN"));
 
         int size = queue.getSize();
-        System.out.println(CLASS_NAME + " - addElementTest(): Initialing - Queue size = " + size);
+        logger.info(CLASS_NAME + " - addElementTest(): Initialing - Queue size = {}", size);
 
         queue.addElement(DefaultValues.PRIMITIVE_INTEGER);
         queue.addElement(DefaultValues.PRIMITIVE_DOUBLE);
@@ -89,8 +97,8 @@ class QueueTest {
         queue.addElement(DefaultValues.OBJECT_ARRAY_STRING);
 
         size = queue.getSize();
-        System.out.println(CLASS_NAME + " - addElementTest(): After adding elements - Queue size = " + size);
-        System.out.println(CLASS_NAME.concat(" - addElementTest(): END"));
+        logger.info(CLASS_NAME + " - addElementTest(): After adding elements - Queue size = {}", size);
+        logger.debug(CLASS_NAME.concat(" - addElementTest(): END"));
         Assertions.assertEquals(QUEUE_MAX_SIZE, size);
     }
 
@@ -101,23 +109,23 @@ class QueueTest {
     @Test
     @Order(3)
     void removeElementTest() {
-        System.out.println(CLASS_NAME.concat(" - removeElementTest(): BEGIN"));
+        logger.debug(CLASS_NAME.concat(" - removeElementTest(): BEGIN"));
 
 //      Parece que o Junit reinicia a instancia depois de terminar os metodos.
 //      Por isso, chamei aqui o metodo addElementTest.
         this.addElementTest();
 
         final int randomIndex = getRandomIndexQueueSize();
-        System.out.println(CLASS_NAME + " - removeElementTest(): randomIndex = " + randomIndex);
+        logger.debug(CLASS_NAME + " - removeElementTest(): randomIndex = {}", randomIndex);
         final Object element = queue.getElement(randomIndex);
 
-        System.out.println(CLASS_NAME + " - removeElementTest(): element = " + element);
+        logger.debug(CLASS_NAME + " - removeElementTest(): element = {}", element);
         final boolean wasRemoved = queue.removeElement(randomIndex);
 
-        System.out.println(CLASS_NAME + " - removeElementTest(): wasRemoved = " + wasRemoved);
+        logger.info(CLASS_NAME + " - removeElementTest(): wasRemoved = {}", wasRemoved);
         final int indexRemoved = queue.searchElement(element);
 
-        System.out.println(CLASS_NAME.concat(" - removeElementTest(): END"));
+        logger.debug(CLASS_NAME.concat(" - removeElementTest(): END"));
         Assertions.assertEquals(Queue.NOT_FOUND, indexRemoved);
     }
 
@@ -128,15 +136,15 @@ class QueueTest {
     @Test
     @Order(4)
     void searchElementTest() {
-        System.out.println(CLASS_NAME.concat(" - searchElementTest(): BEGIN"));
+        logger.debug(CLASS_NAME.concat(" - searchElementTest(): BEGIN"));
 //      Parece que o Junit reinicia a instancia depois de terminar os metodos.
 //      Por isso, chamei aqui o metodo addElementTest.
         this.addElementTest();
         final int randomIndex = getRandomIndexQueueSize();
         final Object element = queue.getElement(randomIndex);
         final boolean isFoundElement = ( queue.searchElement(element) != Queue.NOT_FOUND );
-        System.out.println(CLASS_NAME + " - searchElementTest(): isFoundElement = " + isFoundElement);
-        System.out.println(CLASS_NAME.concat(" - searchElementTest(): END"));
+        logger.info(CLASS_NAME + " - searchElementTest(): isFoundElement = {}", isFoundElement);
+        logger.debug(CLASS_NAME.concat(" - searchElementTest(): END"));
         Assertions.assertTrue(isFoundElement);
     }
 
@@ -147,20 +155,20 @@ class QueueTest {
     @Test
     @Order(5)
     void isEmptyTest() {
-        System.out.println(CLASS_NAME.concat(" - isEmptyTest(): BEGIN"));
+        logger.debug(CLASS_NAME.concat(" - isEmptyTest(): BEGIN"));
 
         final boolean isZeroInitialQueueSize = queue.isEmpty();
-        System.out.println(CLASS_NAME + " - isEmptyTest(): isZeroInitialQueueSize = " + isZeroInitialQueueSize);
+        logger.info(CLASS_NAME + " - isEmptyTest(): isZeroInitialQueueSize = {}", isZeroInitialQueueSize);
 
         this.addElementTest();
         final boolean isFullQueueSize = ( queue.isEmpty() == false );
-        System.out.println(CLASS_NAME + " - isEmptyTest(): isFullQueueSize = " + isFullQueueSize);
+        logger.info(CLASS_NAME + " - isEmptyTest(): isFullQueueSize = {}", isFullQueueSize);
 
-        queue.eraseAllElements();
+        final boolean isErased = queue.eraseAllElements();
         final boolean isErasedQueue = queue.isEmpty();
-        System.out.println(CLASS_NAME + " - isEmptyTest(): isErasedQueue = " + isErasedQueue);
+        logger.info(CLASS_NAME + " - isEmptyTest(): isErasedQueue = {}", isErasedQueue);
 
-        System.out.println(CLASS_NAME.concat(" - isEmptyTest(): END"));
+        logger.debug(CLASS_NAME.concat(" - isEmptyTest(): END"));
         Assertions.assertTrue((isZeroInitialQueueSize && isFullQueueSize && isErasedQueue));
     }
 
@@ -171,23 +179,29 @@ class QueueTest {
     @Test
     @Order(6)
     void containsTest() {
-        System.out.println(CLASS_NAME.concat(" - containsTest(): BEGIN"));
+        logger.debug(CLASS_NAME.concat(" - containsTest(): BEGIN"));
         this.addElementTest();
         final int randomIndex = getRandomIndexQueueSize();
         final Object randomElement = queue.getElement(randomIndex);
-        System.out.println(CLASS_NAME + " - containsTest(): randomElement.getType = " + queue.getObjectType(randomElement));
+        logger.info(CLASS_NAME + " - containsTest(): randomElement.getType = {}", queue.getObjectType(randomElement));
         final boolean isFoundElement = queue.contains(randomElement);
-        System.out.println(CLASS_NAME + " - containsTest(): isFoundElement = " + isFoundElement);
-        System.out.println(CLASS_NAME + " - containsTest(): END");
+        logger.info(CLASS_NAME + " - containsTest(): isFoundElement = {}", isFoundElement);
+        logger.debug(CLASS_NAME + " - containsTest(): END");
         Assertions.assertTrue(isFoundElement);
     }
 
+    /**
+     * Teste 7 - Testar a conversão do valor(es) de um(ns) objeto(s) em string.
+     * Cenário: Tipos primitivos serão convertidos em string, enquanto arrays de objetos terão seus
+     * valores armazenados convertidos e formatados em string.
+     */
     @Test
     @Order(7)
     void toStringTest() {
-        System.out.println(CLASS_NAME.concat(" - toStringTest(): BEGIN"));
+        logger.debug(CLASS_NAME.concat(" - toStringTest(): BEGIN"));
         this.addElementTest();
-        for(Object element : queue.getAllElements()) System.out.println(CLASS_NAME + " - toStringTest(): " + queue.toString(element));
+        for(Object element : queue.getAllElements()) logger.info(CLASS_NAME + " - toStringTest(): {}", queue.toString(element));
+        logger.debug(CLASS_NAME.concat(" - toStringTest(): END"));
     }
 
 

ceb03288263382bdf20f41a27ff2a17e34d5fc1c
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2025-01-18 00:09:11 -0300

    Adicionando javadoc na classe LinkedList e Node

--- src/main/otogamidev/list/LinkedList.java
+++ src/main/otogamidev/list/LinkedList.java
@@ -3,6 +3,11 @@ package main.otogamidev.list;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+/**
+ * Classe responsavel pela implementacao de Lista Encadeada.
+ *
+ * @author henriquematheusalvespereira
+ */
 public class LinkedList {
 
     final String CLASS_NAME = "LinkedList";
@@ -25,9 +30,13 @@ public class LinkedList {
         logger.info("LinkedList() - head = {} | size = {}", this.head, this.size);
     }
 
+    /**
+     * Metodo responsavel pela adicao do no inicial e dos proximos nos
+     * @param element instancia de No a ser adicionado na lista
+     */
     public void append(final Node element) {
         final Node node = element;
-        if(this.size == 0) {
+        if(this.isEmpty()) {
             this.head = node;
         } else {
             this.next.setNextElement(node);
@@ -38,12 +47,34 @@ public class LinkedList {
         logger.info("append() - size = {} | next = {}\n", this.size, this.next);
     }
 
+    /**
+     * Metodo responsavel pela adicao do no inicial e dos proximos nos
+     * @param element instancia de objeto a ser adicionado na lista
+     */
     public void append(final Object element) { this.append(new Node(element)); }
 
+    /**
+     * Metodo responsavel pela obtencao do tamanho da lista encadeada
+     * @return retorna numero inteiro indicando o tamanho da lista
+     */
     public int getSize() { return this.size; }
 
+    /**
+     * Metodo responsavel pela verificacao de tamanho da lista
+     * @return retorna valor true indicando que esta vazia e false indicando que esta cheia
+     */
+    public boolean isEmpty() { return (this.size == 0); }
+
+    /**
+     * Metodo responsavel pela conversão da lista em string
+     * @return retorna conteudo formatado em string
+     */
     @Override
     public String toString() { return "LinkedList { head = " + this.head + " } "; }
 
+    /**
+     * Metodo responsavel pela obtencao do nome da classe
+     * @return retorna o nome da classe em string
+     */
     public String getClassName() { return this.CLASS_NAME; }
 }
--- src/main/otogamidev/list/Node.java
+++ src/main/otogamidev/list/Node.java
@@ -3,6 +3,11 @@ package main.otogamidev.list;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+/**
+ * Classe responsavel pela implementacao do No de Lista Encadeada
+ *
+ * @author henriquematheusalvespereira
+ */
 public class Node extends Object {
 
     final String CLASS_NAME = "Node";
@@ -14,16 +19,40 @@ public class Node extends Object {
 
     public Node(final Object object) { this.element = object; }
 
+    /**
+     * Metodo responsavel pela obtencao do elemento armazenado no No
+     * @return retorna um elemento do tipo objeto
+     */
     public Object getElement() { return element; }
 
+    /**
+     * Metodo responsavel pelo armazenamento do elemento
+     * @param element elemento do tipo objeto
+     */
     public void setElement(final Object element) { this.element = element; }
 
+    /**
+     * Metodo responsavel pela obtencao do proximo No da Lista Encadeada
+     * @return retorna o proximo No da Lista Encadeada
+     */
     public Node getNextElement() { return nextElement; }
 
+    /**
+     * Metodo responsavel pelo armazenamento do proximo No da Lista Encadeada
+     * @param nextObject elemento do tipo No
+     */
     public void setNextElement(final Node nextObject) { this.nextElement = nextObject; }
 
-    public String getClassName() { return this.CLASS_NAME; }
-
+    /**
+     * Metodo responsavel pela conversão da lista em string
+     * @return retorna conteudo formatado em string
+     */
     @Override
     public String toString() { return "Node { nextElement = " + nextElement + ", element = " + element + " } "; }
+
+    /**
+     * Metodo responsavel pela obtencao do nome da classe
+     * @return retorna o nome da classe em string
+     */
+    public String getClassName() { return this.CLASS_NAME; }
 }

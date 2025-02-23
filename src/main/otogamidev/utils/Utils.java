e8cb354a5b44167df4b91ee31a15887d77c64d80
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2025-02-23 16:01:12 -0300

    Implementação da classe Utils

---
+++ src/main/otogamidev/utils/Utils.java
@@ -0,0 +1,29 @@
+package main.otogamidev.utils;
+
+import main.otogamidev.list.LinkedList;
+import main.otogamidev.list.Node;
+
+public class Utils {
+
+    /**
+     * Metodo para gerar numero aleatorio de tamanho de lista
+     * @return Retorna um índice "aleatorio" para que os metodos dos testes abaixos utilizem tipos de objetos diferentes.
+     */
+    public static int getRandomIndex(final int size) { return (int) (Math.random() * size); }
+
+    public static String getListWithoutSpecificIndex(final LinkedList<Object> list, final int ignorePosition) {
+        if(list.isEmpty()) return "[]";
+        final StringBuilder stringBuilder = new StringBuilder("[");
+        final int lastIndex = list.getLastIndex();
+        for(int index = 0; lastIndex >= index; index++) {
+            if(index == ignorePosition) continue;
+
+            final Object element = list.searchElementByPosition(index);
+            stringBuilder.append(element).append(",");
+        }
+//      Estou adicionando uma vírgula para todos os objetos da lista e depois removo o último aqui
+        stringBuilder.replace(stringBuilder.length()-1, stringBuilder.length(),"");
+        stringBuilder.append("]");
+        return stringBuilder.toString();
+    }
+}

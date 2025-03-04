18a6d0eed2e6e2578417c89d6918eadfd439c929
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2025-03-04 02:14:04 -0300

    #5 #6 Implementação do teste unitário appendTest()

--- src/test/otogamidev/vector/VectorTest.java
+++ src/test/otogamidev/vector/VectorTest.java
@@ -21,11 +21,11 @@ public class VectorTest {
     private static final String CLASS_NAME = "VectorTest";
     private final Logger logger = LogManager.getLogger(VectorTest.class);
 
-    private final String linkedListEmpty = "[]";
-    private final String linkedListFull  = "[1,2,3,4,5]";
+    private final String vectorEmpty = "[]";
+    private final String vectorFull[]  = new String[] { "elemento 1", "elemento 2", "elemento 3", "elemento 4", "elemento 5" };
 
-    private final int linkedListEmptySize = 0;
-    private final int linkedListFullSize  = 5;
+    private final int vectorEmptySize = 0;
+    private final int vectorFullSize = 5;
 
     private Vector vector = null;
 
@@ -41,14 +41,41 @@ public class VectorTest {
     @Order(1)
     public void createVectorTest() {
         logger.debug("createVectorTest() - BEGIN");
-        this.vector = new Vector(0);
-        final int initialSize = this.vector.getSize();
-        this.vector = new Vector(5);
-        final int updateSize = this.vector.getSize();
-        Assertions.assertEquals(initialSize, linkedListEmptySize);
-        Assertions.assertEquals(updateSize, linkedListFullSize);
+        this.vector = new Vector(vectorEmptySize);
+        final int initialCapable = this.vector.getElements().length;
+        this.vector = new Vector(vectorFullSize);
+        final int updateCapable = this.vector.getElements().length;
+        Assertions.assertEquals(initialCapable, vectorEmptySize);
+        Assertions.assertEquals(updateCapable, vectorFullSize);
         logger.info("createVectorTest() - SUCCESS");
         logger.debug("createVectorTest() - END");
+    }
+
+    /**
+     * Test 2 - Adicionar elementos no Vetor
+     * Cenário: Instanciar o Vetor com capacidade de 5 elementos, depois adicionar os elementos e
+     * verificar se o tamanho é incrementado
+     */
+    @Test
+    @Order(2)
+    public void appendTest() {
+        logger.debug("appendTest() - BEGIN");
+        this.vector = new Vector(vectorFullSize);
+        try {
+            for (String element : vectorFull) this.vector.append(element);
+        } catch (final IllegalArgumentException illegalArgumentException) {
+            logger.debug("appendTest() - IllegalArgumentException");
+            logger.info("appendTest() - FAILED");
+            illegalArgumentException.getStackTrace();
+        } catch (final Exception exception) {
+            logger.debug("appendTest() - Exception");
+            logger.info("appendTest() - FAILED");
+            exception.getStackTrace();
+        }
+        final int capable = this.vector.getElements().length;
+        Assertions.assertEquals(vectorFullSize, capable);
+        logger.info("appendTest() - SUCCESS");
+        logger.debug("appendTest() - END");
     }
 
 }

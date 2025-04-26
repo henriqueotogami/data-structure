e2607591110e8c17fb5a3a8e4b50de9963668872
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2025-04-26 15:14:36 -0300

    #10 Implementação do menu de opções da classe ListContacts

--- src/main/otogamidev/exercises/ListContacts.java
+++ src/main/otogamidev/exercises/ListContacts.java
@@ -62,7 +62,37 @@ public class ListContacts {
     }
 
     private static int getMenu(final Scanner scan) {
+
+        boolean isValidInput = false;
+        boolean hasOnlyNumbers = false;
+        String input = "";
+        int option = 0;
+
+        while(!isValidInput) {
+
+            printMenu();
+
+            try {
+                input = scan.nextLine();
+                hasOnlyNumbers = input.matches("[0-9]");
+                if(!hasOnlyNumbers) throw new IllegalArgumentException("Utilize apenas números.");
+
+                option = Integer.parseInt(input);
+                isValidInput = (option > -1 && option < 12);
+
+            } catch (final Exception exception) {
+                logger.info("Entrada inválida. Tente novamente.");
+                logger.debug(exception.getStackTrace());
+            } finally {
+                logger.info("Entrada informada = {}\r\n", option);
+            }
+        }
+        return option;
+    }
+
+    private static void printMenu() {
         logger.info("Digite a opção desejada:");
+        logger.info("00 - Sair");
         logger.info("01 - Adiciona contato no final do vetor");
         logger.info("02 - Adiciona contato em uma posicão específica do vetor");
         logger.info("03 - Obtém contato de uma posição específica do vetor");
@@ -70,6 +100,21 @@ public class ListContacts {
         logger.info("05 - Consulta o último índice do contato no vetor");
         logger.info("06 - Verifica a existência do contato no vetor");
         logger.info("07 - Remove o contato por posição específica no vetor");
-        logger.info("08 - Exclui contato do vetor");
+        logger.info("08 - Excluir contato do vetor");
+        logger.info("09 - Verificar o tamanho do vetor");
+        logger.info("10 - Excluir todos os contatos do vetor");
+        logger.info("11 - Imprime o vetor\r\n");
+    }
+
+    public static void main(String[] args) {
+        logger.info("Programa iniciado");
+
+        int option = 1;
+
+        while(option != 0) {
+            option = getMenu(scanner);
+        }
+
+        logger.info("Programa terminado");
     }
 }

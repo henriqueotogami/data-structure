2e428748d3055776cbe6a03932ef624c5bca75f6
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2025-05-04 18:57:55 -0300

    #11 implementação inicial da classe de testes unitários StackTest

--- src/test/otogamidev/stack/StackTest.java
+++ src/test/otogamidev/stack/StackTest.java
@@ -1,4 +1,35 @@
 package test.otogamidev.stack;
 
+import main.otogamidev.stack.Stack;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+
+/**
+ * Classe responsável pelos testes da classe Stack.
+ *
+ * @author henriquematheusalvespereira
+ */
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class StackTest {
+
+//    =================================================================================================================
+//    Variáveis dos Testes Unitarios - Inicio
+//    =================================================================================================================
+
+    /** Nome da Classe StackTest */
+    private static final String CLASS_NAME = "StackTest";
+    private final Logger logger = LogManager.getLogger(StackTest.class);
+
+//    =================================================================================================================
+//    Variáveis dos Testes Unitarios - Fim
+//    =================================================================================================================
+
+    public static void main(String[] args) {
+
+        final Stack<Integer> stack = new Stack<Integer>();
+        System.out.println(stack);
+        System.out.println(stack.getSize());
+    }
 }

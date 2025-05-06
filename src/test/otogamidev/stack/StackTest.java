37dc2f572f05ec648c9287fbb694a69eeba41818
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2025-05-05 23:27:07 -0300

    #11 Implementação do teste unitário do método push() da classe Stack

--- src/test/otogamidev/stack/StackTest.java
+++ src/test/otogamidev/stack/StackTest.java
@@ -3,8 +3,9 @@ package test.otogamidev.stack;
 import main.otogamidev.stack.Stack;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
+
+import java.util.Arrays;
 
 /**
  * Classe responsável pelos testes da classe Stack.
@@ -26,10 +27,30 @@ public class StackTest {
 //    Variáveis dos Testes Unitarios - Fim
 //    =================================================================================================================
 
-    public static void main(String[] args) {
-
+    @Test
+    @Order(1)
+    public void initEmptyStackTest() {
         final Stack<Integer> stack = new Stack<Integer>();
-        System.out.println(stack);
-        System.out.println(stack.getSize());
+        final String emptyStack = "[null, null, null, null, null, null, null, null, null, null]";
+        Assertions.assertEquals(emptyStack, stack.toString());
+        Assertions.assertEquals(0, stack.getSize());
+        logger.info("initEmptyStackTest() - SUCESS");
+    }
+
+    @Test
+    @Order(2)
+    public void pushTest(){
+        final int[] array = new int[10];
+        final Stack<Integer> stack = new Stack<Integer>();
+
+        for(int index = 0; array.length > index; index++) {
+            final int value = index + 1;
+            array[index] = value;
+            stack.push(value);
+        }
+
+        Assertions.assertEquals(Arrays.toString(array), stack.toString());
+        Assertions.assertEquals(array.length, stack.getSize());
+        logger.info("pushTest() - SUCESS");
     }
 }

435655aa4d3c4f9d5f8423d21001d9e96af3b627
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2025-05-19 10:00:00 -0300

    #15 Início da implementação da classe de Pacientes

--- src/main/otogamidev/exercises/QueuePriority.java
+++ src/main/otogamidev/exercises/QueuePriority.java
@@ -1,4 +1,40 @@
 package main.otogamidev.exercises;
 
-public class QueuePriority {
+import main.otogamidev.queue.QueueStatic;
+
+public class QueuePriority<T> extends QueueStatic<T> {
+
+    public void addElement(final T element) {
+
+    }
+
+    public static class Patient {
+
+        private String name;
+        private int priority;
+
+        public Patient(String name, int priority) {
+            this.name = name;
+            this.priority = priority;
+        }
+
+        public Patient() { }
+
+        public String getName() { return name; }
+
+        public void setName(final String name) { this.name = name; }
+
+        public int getPriority() { return priority; }
+
+        public void setPriority(final int priority) { this.priority = priority; }
+    }
+
+
+    public static void main(String[] args) {
+        final QueuePriority<Integer> queuePriority = new QueuePriority<>();
+        queuePriority.addElement(1);
+        queuePriority.addElement(3);
+        queuePriority.addElement(2);
+    }
+
 }

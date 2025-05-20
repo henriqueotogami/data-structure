95ab3af5574b466fd2fe2e79a1287ecbf895546c
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2025-05-20 01:05:36 -0300

    #15 Implementação da Fila com prioridade

--- src/main/otogamidev/exercises/QueuePriority.java
+++ src/main/otogamidev/exercises/QueuePriority.java
@@ -5,13 +5,25 @@ import main.otogamidev.queue.QueueStatic;
 public class QueuePriority<T> extends QueueStatic<T> {
 
     public void addElement(final T element) {
+        final Comparable<T> key = (Comparable<T>) element;
+        int index = 0;
+        for(index = 0;this.size > index; index++) {
+            if(Patient.PRIORITY_EQUALS > key.compareTo(this.elements[index])) {
+                break;
+            }
+        }
 
+        this.append(index, element);
     }
 
-    public static class Patient {
+    public static class Patient implements Comparable {
 
         private String name;
         private int priority;
+
+        private static final int PRIORITY_EQUALS    = 0;
+        private static final int PRIORITY_GREATER   = 1;
+        private static final int PRIORITY_MINOR     = -1;
 
         public Patient(String name, int priority) {
             this.name = name;
@@ -27,14 +39,36 @@ public class QueuePriority<T> extends QueueStatic<T> {
         public int getPriority() { return priority; }
 
         public void setPriority(final int priority) { this.priority = priority; }
+
+        public int compareTo(final Patient patient) {
+            final int patientPriority = patient.getPriority();
+
+            if(this.priority == patientPriority) return PRIORITY_EQUALS;
+
+            return (this.priority > patientPriority) ? PRIORITY_GREATER : PRIORITY_MINOR;
+        }
+
+        @Override
+        public int compareTo(final Object object) {
+            return this.compareTo((Patient) object);
+        }
     }
 
 
     public static void main(String[] args) {
         final QueuePriority<Integer> queuePriority = new QueuePriority<>();
         queuePriority.addElement(1);
+        queuePriority.addElement(18);
         queuePriority.addElement(3);
+        queuePriority.addElement(23);
+        queuePriority.addElement(9);
+        queuePriority.addElement(5);
+        queuePriority.addElement(15);
         queuePriority.addElement(2);
+        queuePriority.addElement(78);
+        queuePriority.addElement(0);
+        System.out.println(queuePriority.toString());
+//        [0, 1, 2, 3, 5, 9, 15, 18, 23, 78]
     }
 
 }
---
+++ src/test/otogamidev/exercises/QueuePriorityTest.java
@@ -0,0 +1,4 @@
+package test.otogamidev.exercises;
+
+public class QueuePriorityTest {
+}

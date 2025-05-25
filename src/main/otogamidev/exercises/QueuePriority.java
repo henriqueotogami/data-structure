9f593527c11dbb3cd001c570955da29100cc1969
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2025-05-25 16:17:15 -0300

    #15 Ajustes na validação de comparação de elementos no enqueue() e no compareTo()

--- src/main/otogamidev/exercises/QueuePriority.java
+++ src/main/otogamidev/exercises/QueuePriority.java
@@ -1,29 +1,38 @@
 package main.otogamidev.exercises;
 
 import main.otogamidev.queue.QueueStatic;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class QueuePriority<T> extends QueueStatic<T> {
 
-    public void addElement(final T element) {
-        final Comparable<T> key = (Comparable<T>) element;
+    /** Nome da Classe QueuePriority */
+    private static final String CLASS_NAME = "QueuePriority";
+    private static final Logger logger = LogManager.getLogger(QueuePriority.class);
+
+    @Override
+    public void enqueue(final T element) {
+        final int newElementHasPriority = 1;
+        final Comparable<T> newElement = (Comparable<T>) element;
         int index = 0;
-        for(index = 0;this.size > index; index++) {
-            if(Patient.PRIORITY_EQUALS > key.compareTo(this.elements[index])) {
+        for(; this.size > index; index++) {
+            final int compared = newElement.compareTo(this.elements[index]);
+            if(compared != newElementHasPriority) {
+//                logger.info("Novo elemento tem maior prioridade = " + compared);
                 break;
             }
         }
-
         this.append(index, element);
     }
 
-    public static class Patient implements Comparable {
+    public static class Patient<T> implements Comparable {
 
         private String name;
         private int priority;
 
-        public static final int PRIORITY_EQUALS    = 0;
-        public static final int PRIORITY_GREATER   = 1;
-        public static final int PRIORITY_MINOR     = -1;
+        public static final int PRIORITY_NONE       = 0;
+        public static final int PRIORITY_GREATER    = 1;
+        public static final int PRIORITY_MINOR      = -1;
 
         public Patient(String name, int priority) {
             this.name = name;
@@ -40,34 +49,34 @@ public class QueuePriority<T> extends QueueStatic<T> {
 
         public void setPriority(final int priority) { this.priority = priority; }
 
-        public int compareTo(final Patient patient) {
-            final int patientPriority = patient.getPriority();
-
-            if(this.priority == patientPriority) return PRIORITY_EQUALS;
-
-            return (this.priority > patientPriority) ? PRIORITY_GREATER : PRIORITY_MINOR;
+        public int compareTo(final Patient otherPatient) {
+//            if(this.priority ==  otherPatient.getPriority()) return PRIORITY_NONE;
+//            return (this.priority >  otherPatient.getPriority()) ? PRIORITY_GREATER : PRIORITY_MINOR;
+//            logger.info("CompareTo() - " + otherPatient.getName());
+            return Integer.compare(this.priority, otherPatient.getPriority());
         }
 
         @Override
         public int compareTo(final Object object) {
             return this.compareTo((Patient) object);
         }
+
     }
 
 
     public static void main(String[] args) {
         final QueuePriority<Integer> queuePriority = new QueuePriority<>();
-        queuePriority.addElement(1);
-        queuePriority.addElement(18);
-        queuePriority.addElement(3);
-        queuePriority.addElement(23);
-        queuePriority.addElement(9);
-        queuePriority.addElement(5);
-        queuePriority.addElement(15);
-        queuePriority.addElement(2);
-        queuePriority.addElement(78);
-        queuePriority.addElement(0);
-        System.out.println(queuePriority.toString());
+        queuePriority.enqueue(1);
+        queuePriority.enqueue(18);
+        queuePriority.enqueue(3);
+        queuePriority.enqueue(23);
+        queuePriority.enqueue(9);
+        queuePriority.enqueue(5);
+        queuePriority.enqueue(15);
+        queuePriority.enqueue(2);
+        queuePriority.enqueue(78);
+        queuePriority.enqueue(0);
+        logger.info(queuePriority.toString());
 //        [0, 1, 2, 3, 5, 9, 15, 18, 23, 78]
     }
 

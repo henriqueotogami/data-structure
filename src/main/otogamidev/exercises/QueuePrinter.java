b2e0a95392db8fecb3b25f5ae529d8fde7d7b647
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2025-05-25 16:18:51 -0300

    #15 Ajustes de enfileiramento, adição de logs e implementação da classe Comparable

--- src/main/otogamidev/exercises/QueuePrinter.java
+++ src/main/otogamidev/exercises/QueuePrinter.java
@@ -1,10 +1,17 @@
 package main.otogamidev.exercises;
 
 import main.otogamidev.queue.QueueStatic;
+import main.otogamidev.utils.Utils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class QueuePrinter {
 
-    public static class Document {
+    /** Nome da Classe QueuePrinter */
+    private static final String CLASS_NAME = "QueuePrinter";
+    private static final Logger logger = LogManager.getLogger(QueuePrinter.class);
+    
+    public static class Document implements Comparable {
 
         private String name = "Document";
         private int numberOfPages = 0;
@@ -17,32 +24,32 @@ public class QueuePrinter {
         public String getName() { return name; }
 
         public int getNumberOfPages() { return numberOfPages; }
+
+        @Override
+        public int compareTo(Object o) {
+            // Implementei a classe Comparable na Document, porque o metodo enqueue precisa do CompareTo
+            return 0;
+        }
     }
 
     public static void main(String[] args) {
 
         final QueueStatic<Document> queuePrinter = new QueuePriority<>();
-        queuePrinter.enqueue(new Document("Document-01.pdf", 1));
-        queuePrinter.enqueue(new Document("Document-02.pdf", 9));
-        queuePrinter.enqueue(new Document("Document-03.pdf", 12));
-        queuePrinter.enqueue(new Document("Document-04.pdf", 5));
-        queuePrinter.enqueue(new Document("Document-05.pdf", 7));
-        queuePrinter.enqueue(new Document("Document-06.pdf", 2));
-        queuePrinter.enqueue(new Document("Document-07.pdf", 10));
-        queuePrinter.enqueue(new Document("Document-08.pdf", 8));
-        queuePrinter.enqueue(new Document("Document-09.pdf", 15));
-        queuePrinter.enqueue(new Document("Document-10.pdf", 11));
+        for(int index = 0; 10 > index; index++) {
+            final int randomNumberOfPages = Utils.getRandomIndex(30);
+            queuePrinter.enqueue(new Document(("Document-" + (index+1) + ".pdf"), randomNumberOfPages));
+        }
 
         while(!queuePrinter.isEmpty()) {
             final Document document = queuePrinter.dequeue();
-            System.out.println("document = " + document.getName());
+            logger.info("Imprimindo: {} | Número de páginas = {}", document.getName(), document.getNumberOfPages());
             try {
                 Thread.sleep(500 * document.getNumberOfPages());
             } catch (final InterruptedException interruptedException) {
                 interruptedException.getStackTrace();
             }
         }
-        System.out.println("Todos os documentos foram impressos.");
+        logger.info("Todos os documentos foram impressos.");
     }
 
 }

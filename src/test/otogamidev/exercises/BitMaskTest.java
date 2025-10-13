ba4cbea31ff97efc0f6297228e9f74280ae98af4
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2025-10-12 23:53:08 -0300

    #20 Implementação do Teste Unitário da Tradução da Máscara de Bits

--- src/test/otogamidev/exercises/BitMaskTest.java
+++ src/test/otogamidev/exercises/BitMaskTest.java
@@ -1,4 +1,31 @@
 package test.otogamidev.exercises;
 
+import main.otogamidev.exercises.BitMask;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.*;
+
+/**
+ * Classe de Testes para a classe BitMask
+ * @author henriquematheusalvespereira
+ */
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class BitMaskTest {
+
+    private static final String CLASS_NAME = "BitMaskTest";
+    private final Logger logger = LogManager.getLogger(BitMaskTest.class);
+
+    @Test
+    @Order(1)
+    public void validateActivateSensors() {
+        logger.debug("validateActivateSensors() - BEGIN");
+        final int received              = 0x7F;
+        final BitMask bitMask           = new BitMask();
+        final String[] validatedSensors = bitMask.validateActivatedSensors(received);
+        for(String sensor : validatedSensors) {
+            logger.info(sensor);
+        }
+        Assertions.assertEquals(8, validatedSensors.length);
+        logger.debug("validateActivateSensors() - END");
+    }
 }
